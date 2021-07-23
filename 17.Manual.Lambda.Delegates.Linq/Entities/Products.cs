e15b7f675976df35af94a46a49c78b02955019d1
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-23 16:55:51 -0300

    Adicionar arquivos de projeto.

---
+++ 17.Manual.Lambda.Delegates.Linq/Entities/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Products {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price) {
+            Name = Name;
+            Price = Price;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
---
+++ 17.Manual.Lambda.Delegates.Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+namespace _17.Manual.Lambda.Delegates.Linq {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Products> list = new List<Products>();
+
+            list.Add(new Products("TV", 900.00));
+            list.Add(new Products("Notebook", 1200.00));
+            list.Add(new Products("Tablet", 450.00));
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Products p in list) {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
